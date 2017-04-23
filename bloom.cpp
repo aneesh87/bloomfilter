51f3e305ef68addbbfa416ec423a775db77012e7
agupta27 <agupta27@ncsu.edu>
2017-04-23 06:05:44 -0400

    some refactor changes

--- bloom.cpp
+++ bloom.cpp
@@ -1,123 +1,118 @@
-/*
-	Command to execute the program: g++ srajago3.cpp -o bloom -g -w
-
-*/
-
-#include <iostream>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 #include <bitset>
+#include <iostream>
 #include <cstdlib>
 
 #define ARR_LENGTH 8
-#define LENGTH 10
-/* Structure for the bloom filter*/
-typedef struct bf_t{
-	std::bitset<2000000> bloomSet;
-}bf_t;
+#define BLOOMSETSIZE 2000000
 
-int prime[ARR_LENGTH] = {3,5,7,11,13,17,19,23};
+typedef struct bf_t{
+
+  std::bitset<BLOOMSETSIZE> bloomFilterSet;
+
+} bf_t;
+
+unsigned int primes[ARR_LENGTH] = { 1540655027, 1840695761, 1740683437, 1840701563, 
+                                    1640680681, 1940700029, 2100003041, 2040668849
+                                  };
 
 /*
-	Hash Function
-//-----------------------------------------------------------------------------
-// MurmurHash2, by Austin Appleby
+	Hash Function used 
+   Reference MurmurHash2 -- Austin Appleby
+   https://github.com/aappleby/smhasher/blob/master/src/MurmurHash2.cpp
 */
 
+int MurmurHash2(const void * key, int len, unsigned int seed) {
 
-int MurmurHash2 ( const void * key, int len, unsigned int seed )
-{
-	// 'm' and 'r' are mixing constants generated offline.
-	// They're not really 'magic', they just happen to work well.
-
-	const int m = 0x5bd1e995;
-	const int r = 24;
-
-	// Initialize the hash to a 'random' value
-
-	int h = seed ^ len;
-
-	// Mix 4 bytes at a time into the hash
-
-	const unsigned char * data = (const unsigned char *)key;
-
-	while(len >= 4)
-	{
+   // 'm' and 'r' are mixing constants generated offline.
+   // They're not really 'magic', they just happen to work well.
+   const int m = 1540483477;
+   const int r = 24;
+   // Initialize the hash to a 'random' value
+   int h = seed ^ len;
+   // Mix 4 bytes at a time into the hash
+   const unsigned char * data = (const unsigned char *)key;
+   while(len >= 4) {
 		int k = *(int *)data;
-
 		k *= m; 
 		k ^= k >> r; 
 		k *= m; 
 		
 		h *= m; 
 		h ^= k;
-
 		data += 4;
 		len -= 4;
 	}
-	
 	// Handle the last few bytes of the input array
-
-	switch(len)
-	{
+	switch(len) {
       case 3: h ^= data[2] << 16;
-
       case 2: h ^= data[1] << 8;
-
       case 1: h ^= data[0];
               h *= m;
 	};
-
 	// Do a few final mixes of the hash to ensure the last few
 	// bytes are well-incorporated.
-
 	h ^= h >> 13;
 	h *= m;
 	h ^= h >> 15;
-
-	return h%2000000;
-} 
-
-
-/* creates a Bloom filter */
-bf_t * create_bf(){
-	bf_t *bloomFilter = new bf_t[ARR_LENGTH];
-	
-	for(int i=0; i<ARR_LENGTH; i++){
-		//bloomFilter[i].bloomSet.set();
-		//bloomFilter[i].bloomSet.flip();
-		bloomFilter[i].bloomSet.reset();
-	}	 
-
-	return bloomFilter;	
+	return h%BLOOMSETSIZE;
 }
 
+/*
+ * Function: reset_bloom
+ *
+ * Function to reset all bits in bloom filter to 0
+ *
+ * Return Value: None
+ */
 
-/* inserts the string *s into the Bloom filter *b */
-void insert_bf(bf_t *b, char *s){
-	int length = strlen(s);
-	void *str = (void *) s;
-	int i;
-	for(i=0; i<ARR_LENGTH; i++){
-		b[i].bloomSet.set(MurmurHash2 (str, length, prime[i]));
-	}
-
+void reset_bloom(bf_t * t) {
+     for(int j=0; j<ARR_LENGTH; j++)
+        t[j].bloomFilterSet.reset();
+}
+/* 
+ * Function: create_bf()
+ *
+ * Function to create a new Bloom filter 
+ * 
+ * Return Value: Reference to newly created bloom filter
+ */
+bf_t * create_bf() {
+   bf_t * BloomFilter = new bf_t[ARR_LENGTH];
+   reset_bloom(BloomFilter); 
+   return BloomFilter;	
 }
 
-/* returns 1 if the string *q is accepted by the Bloom
-filter, and 0 else.*/
-int is_element(bf_t *b, char *q){
-	int len = strlen(q);
-	void *str = (void *) q;
-	int i;
-	for(i=0; i<ARR_LENGTH; i++){
-		if(!b[i].bloomSet.test(MurmurHash2 (str, len, prime[i]))){
-			return 0;
-		}
+/* 
+ * Function: insert_bf
+ *
+ * Function to insert the input string into the given Bloom filter 
+ *
+ * Return Value: None
+ */
+void insert_bf(bf_t *b, char *s) {
+	for(int j=0; j<ARR_LENGTH; j++)
+   { 
+     int pos = MurmurHash2(s, strlen(s), primes[j]);
+	  b[j].bloomFilterSet.set(pos);
 	}
+}
 
+/* 
+ * Function: is_element
+ * Function to test whether the given string is present in bloom filter.
+ * 
+ * Return Value: If in any of 8 array bloom element sets, the bit for string 
+ * is not set it returns 0, otherwise it will return 1
+ */  
+int is_element(bf_t *b, char *q) {
+	for(int j=0; j<ARR_LENGTH; j++) 
+   {  
+      int pos = MurmurHash2(q, strlen(q), primes[j]);
+	   if(b[j].bloomFilterSet.test(pos)==0) return 0;
+	}
 	return 1;
-
 }
 
 void sample_string_A(char *s, long i)
