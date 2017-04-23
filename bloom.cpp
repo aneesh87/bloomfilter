3c73add6c68f61baff4225435d9249b5948913a8
Ahmad Saad Khan <akhan7@ncsu.edu>
2017-04-22 20:59:38 -0400

    added test cases

--- bloom.cpp
+++ bloom.cpp
@@ -119,8 +119,6 @@ int is_element(bf_t *b, char *q){
 
 }
 
-
-
 void sample_string_A(char *s, long i)
 {  s[0] = (char)(1 + (i%254));
    s[1] = (char)(1 + ((i/254)%254));
@@ -169,11 +167,12 @@ void sample_string_E(char *s, long i)
 }
 
 
+
 int main()
 {  long i,j; 
    bf_t * bloom;
    bloom = create_bf();
-   printf("Bloom Filter created\n");
+   printf("Created Filter\n");
    for( i= 0; i< 1450000; i++ )
    {  char s[8];
       sample_string_A(s,i);
@@ -195,32 +194,32 @@ int main()
    {  char s[8];
       sample_string_A(s,i);
       if( is_element( bloom, s ) != 1 )
-	{  printf("found negative error (1)\n"); exit(0); }
+   {  printf("found negative error (1)\n"); exit(0); }
    }
    for( i= 0; i< 500000; i++ )
    {  char s[7];
       sample_string_B(s,i);
       if( is_element( bloom, s ) != 1 )
-	{  printf("found negative error (2)\n"); exit(0); }
+   {  printf("found negative error (2)\n"); exit(0); }
    }
    for( i= 0; i< 50000; i++ )
    {  char s[6];
       sample_string_C(s,i);
       if( is_element( bloom, s ) != 1 )
-	{  printf("found negative error (3)\n"); exit(0); }
+   {  printf("found negative error (3)\n"); exit(0); }
    }
    j = 0;
    for( i= 0; i< 500000; i++ )
    {  char s[8];
       sample_string_D(s,i);
       if( is_element( bloom, s ) != 0 )
-	j+=1;
+   j+=1;
    }
    for( i= 0; i< 500000; i++ )
    {  char s[7];
       sample_string_E(s,i);
       if( is_element( bloom, s ) != 0 )
-	j+=1;
+   j+=1;
    }
    printf("Found %d positive errors out of 1,000,000 tests.\n",j);
    printf("Positive error rate %f\%.\n", (float)j/10000.0);
